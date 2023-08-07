91bf8b02fdd320077028dfa15eadc6f650971b7b
ChoiBoHyeon <cb7856@naver.com>
2023-08-07 17:23:18 +0900

    first commit

---
+++ Lecture/CallStack.js
@@ -0,0 +1,16 @@
+function first() {
+    second();
+    console.log('첫 번째')
+}
+
+function second() {
+    thrid();
+    console.log('두 번쨔')
+}
+
+function thrid() {
+    console.log('세 번째')
+}
+
+first();
+console.log('test');
