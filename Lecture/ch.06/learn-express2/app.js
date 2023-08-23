2660c81177b295e9ebdd68ebead4bd1fa4b5f801
ChoiBoHyeon <cb7856@naver.com>
2023-08-23 16:37:25 +0900

    JS code-[morgan]

--- Lecture/ch.06/learn-express2/app.js
+++ Lecture/ch.06/learn-express2/app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 
 const app = express()
 
 app.set('port', process.env.PORT || 3000);
 
+// dev : 개발시 , combined : 배포시
+app.use(morgan('combined'));
+
+/*
+// next -> 중복을 줄이기 위한 함수
 app.use((req, res, next) => {
     console.log('미들웨어 사용 중');
     next();
 });
 
-/*
+
 // route parameter 라우트 매개변수
 app.get('/category/:name', (req,res) => {
     res.send(`hello ${req.params.name}`);
@@ -31,8 +37,8 @@ app.get('/get',(req, res) => {
 
 // 오류 처리 및 Http Status 관리
 app.use((req, res, next) => {
-    res.status(200).send('페이지를 찾을 수 없습니다.');
-})
+    res.status(200).send('페이지를 찾을 수 없습니다.(오류 제어 테스트)');
+});
 
 app.listen(3000, () => {
     console.log('express 서버 실행')
