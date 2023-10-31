0e4231a3c34cf9159819c55dea607b2951f89338
ChoiBoHyeon <cb7856@naver.com>
2023-10-31 18:20:17 +0900

    JS code-[Nodebird - hashtagCode]

--- Lecture/ch.09/controllers/page.js
+++ Lecture/ch.09/controllers/page.js
@@ -1,17 +1,29 @@
- exports.renderProfile = (req, res, next) => {
-    // 서비스를 호출
-    res.render('profile', {title: '내 정보 - NodeBird'});
- };
+const { User, Post } = require('../models');
 
- exports.renderJoin = (req, res, next) => {
-    res.render('join', {title : '회원 가입 - NodeBird'});
- };
+exports.renderProfile = (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
+};
 
- exports.renderMain = (req, res, next) => {
+exports.renderJoin = (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+};
+
+exports.renderMain = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
     res.render('main', {
-        title : 'NodeBird',
-        twits : [],
-    })
- };
-
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+}
  // 라우터 -> 컨트롤러 -> 서비스 (요청, 응답 모른다.)
