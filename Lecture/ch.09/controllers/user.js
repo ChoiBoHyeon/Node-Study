ff9bcbbb8a817c39d15330bf8edef0ddcf8e751f
ChoiBoHyeon <cb7856@naver.com>
2023-11-20 16:18:13 +0900

    JS code-[TestCodeRefectoring]

--- Lecture/ch.09/controllers/user.js
+++ Lecture/ch.09/controllers/user.js
@@ -7,11 +7,9 @@ exports.follow = async (req, res, next) => {
     if (result === 'ok') { // req.user.id가 followerId, req.params.id가 followingId
       res.send('success');
     } else if (result === 'no user') {
-        // console.log('여기가 문제네');
         res.status(404).send('no user');
     }
   } catch (error) {
-    // console.log('여기가 문제네2');
     console.error(error);
     next(error);
   }
