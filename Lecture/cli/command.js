08840ce2cb7d9659bb08eaff24ce3a15d44b5911
ChoiBoHyeon <cb7856@naver.com>
2023-12-19 19:29:40 +0900

    JS code-[cliProject-useInqrirerModul]

--- Lecture/cli/command.js
+++ Lecture/cli/command.js
@@ -6,9 +6,9 @@ const inquirer = require('inquirer');
 
 const htmlTemplate = `
 <!DOCTYPE html>
-  <html>
+<html>
   <head>
-    <meta chart="utf-8" />
+    <meta charset="utp-8" />
     <title>Template</title>
   </head>
   <body>
@@ -18,32 +18,8 @@ const htmlTemplate = `
 </html>
 `;
 
-const routerTemplate = `
-const express = require('express');
-const router = express.Router();
- 
-router.get('/', (req, res, next) => {
-   try {
-     res.send('ok');
-   } catch (error) {
-     console.error(error);
-     next(error);
-   }
-});
- 
-module.exports = router;
-`;
-
-const exist = (dir) => {
-  try {
-    fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-const mkdirp = (dir) => {
+// 파일 만들기(빈파일)
+const mkdirp = (dir) => { // 경로 생성 함수
   const dirname = path
     .relative('.', path.normalize(dir))
     .split(path.sep)
@@ -56,26 +32,37 @@ const mkdirp = (dir) => {
   });
 };
 
+// 해당 파일이 존재하는지 검사
+const exist = (dir) => {
+  try {
+    fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+    return true;
+  } catch(e) {
+    return false;
+  }
+}
+
+// 파일 만들기(템플릿 사용하여 원하는 양식의 파일)
 const makeTemplate = (type, name, directory) => {
   mkdirp(directory);
   if (type === 'html') {
     const pathToFile = path.join(directory, `${name}.html`);
     if (exist(pathToFile)) {
-      console.error('이미 해당 파일이 존재합니다');
+      console.error('이미 해당 파일이 존재합니다.')
     } else {
       fs.writeFileSync(pathToFile, htmlTemplate);
-      console.log(pathToFile, '생성 완료');
+      console.log(pathToFile,' 생성 완료')
     }
   } else if (type === 'express-router') {
-    const pathToFile = path.join(directory, `${name}.js`);
+    const pathToFile = path.join(directory, `${name}.js`)
     if (exist(pathToFile)) {
-      console.error('이미 해당 파일이 존재합니다');
-    } else {
+      console.error('이미 해당 파일이 존재합니다.')
+    }  else {
       fs.writeFileSync(pathToFile, routerTemplate);
-      console.log(pathToFile, '생성 완료');
+      console.log(pathToFile,' 생성 완료')
     }
   } else {
-    console.error('html 또는 express-router 둘 중 하나를 입력하세요.');
+    console.error('html 또는 express-router 둘 중 하나를 입력하세요.')
   }
 };
 
@@ -85,47 +72,48 @@ program
 
 program
   .command('template <type>')
-  .usage('<type> --filename [filename] --path [path]')
+  .usage('<type> --filenmae [filename --path [path]')
   .description('템플릿을 생성합니다.')
   .alias('tmpl')
-  .option('-f, --filename [filename]', '파일명을 입력하세요.', 'index')
+  .option('-f, --filename [filename]', '파일명을 입력하세요', 'index')
   .option('-d, --directory [path]', '생성 경로를 입력하세요', '.')
-  .action((type, options, command) => {
+  .action((type, options) => {
+    console.log(type, options.filename, options.directory);
     makeTemplate(type, options.filename, options.directory);
   });
 
-program
-  .action((options, command) => {
-    if (command.args.length !== 0) {
-      console.log('해당 명령어를 찾을 수 없습니다.');
-      program.help();
-    } else {
-      inquirer.prompt([{
-        type: 'list',
-        name: 'type',
-        message: '템플릿 종류를 선택하세요.',
-        choices: ['html', 'express-router'],
-      }, {
-        type: 'input',
-        name: 'name',
-        message: '파일의 이름을 입력하세요.',
-        default: 'index',
-      }, {
-        type: 'input',
-        name: 'directory',
-        message: '파일이 위치할 폴더의 경로를 입력하세요.',
-        default: '.',
-      }, {
-        type: 'confirm',
-        name: 'confirm',
-        message: '생성하시겠습니까?',
-      }])
-        .then((answers) => {
-          if (answers.confirm) {
-            makeTemplate(answers.type, answers.name, answers.directory);
-            console.log('터미널을 종료합니다.');
-          }
+  program
+    .action((options, command) => {
+      if (command.args.length !== 0) {
+        console.log('해당 명령어를 찾을 수 없습니다.')
+        program.help();
+      } else {
+        inquirer.prompt([{
+          type: 'list',
+          name: 'type',
+          message: '템플릿 종류를 선택하세요.',
+          choices: ['html', 'express-router'],
+        }, {
+          type: 'input',
+          name: 'name',
+          message: '파일의 이름을 입력하세요.',
+          default: 'index', 
+        }, {
+          type: 'input',
+          name: 'directory',
+          message: '파일이 위치할 폴더의 경로를 입력하세요.',
+          default: '.'
+        }, {
+          type: 'confirm',
+          name: 'confirm',
+          message: '생성하시겠습니까?',
+        }])
+          .then((answers) => {
+            if (answers.confirm) {
+              makeTemplate(answers.type, answers.name, answers.directory)
+              console.log('터미널을 종료합니다.')
+            }
         });
-    }
-  })
-  .parse(process.argv);
+      }
+    })
+    .parse(process.argv);
