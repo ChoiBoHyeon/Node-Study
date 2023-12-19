5f40bc9c51eec0cc6ae7203bf98fc31031bc2ce7
ChoiBoHyeon <cb7856@naver.com>
2023-12-19 16:18:18 +0900

    JS code-[cliProject-fileType/Name/DirSetting]

--- Lecture/cli/template.js
+++ Lecture/cli/template.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const reqdline = require('readline');
 
-// npx cli html main .
-const type = process.argv[2];
-const name = process.argv[3];
-const directory = process.argv[4] || '.'
+let rl;
+let type = process.argv[2];
+let name = process.argv[3];
+let directory = process.argv[4] || '.'
 
 const htmlTemplate = `
 <!DOCTYPE html>
@@ -53,6 +54,7 @@ const mkdirp = (dir) => { // 경로 생성 함수
 const exist = (dir) => {
   try {
     fs.accessSync(dir, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+    return true;
   } catch(e) {
     return false;
   }
@@ -82,12 +84,50 @@ const makeTemplate = () => {
   }
 };
 
+// 파일 type 설정
+const typeAnswer = (answer) => {
+  if (answer !== 'html' && answer !== 'express-router') {
+    console.clear();
+    console.log('html 또는 express-router만 지원합니다. : ');
+    return rl.question('어떤 템플랫이 필요합니까? : ', typeAnswer);
+  }
+  type = answer;
+  return rl.question('파일명을 설정하세요. : ', nameAnswer);
+}
+
+// 파일 name 설정
+const nameAnswer = (answer) => {
+  if (!answer || !answer.trim()) {
+    console.clear();
+    console.log('파일 이름을 반드시 설정해야합니다. : ');
+    return rl.question('파일명을 설정하세요. : ', typeAnswer);
+  }
+  name = answer;
+  return rl.question('저장할 경로를 설정하세요. 하지않을 시 현재경로에 생성됩니다. : ', dirAnswer);
+}
+
+// 파일 dir 설정
+const dirAnswer = (answer) => {
+  directory = answer?.trim() || '.';
+  rl.close();
+  makeTemplate();
+}
+
 const program = () => {
   if(!type || !name) {
     console.error('사용방법: cli html|express-router 파일명 [생성경로]')
+    rl = reqdline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    console.clear();
+    rl.question('어떤 템플릿이 필요하십니까? : ', typeAnswer);
   } else {
     makeTemplate();
   }
 };
+// Termenal 명렁어 예시
+// npx cli html main pubilc/html
+// npx cli express-router main pubilc/html
 
 program();
